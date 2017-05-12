acf4b59008a0dd24dc1f9f56deb4f6e1986b2e58
heaenwie <marta.eleonora.ciesielska@gmail.com>
2017-05-12 08:52:43 +0200

    Rest of the tasks

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+
+  def initialize()
+  end
+
+  def title #nie kumam, dlaczego muszą być dwie metody :(
+    @title
+  end
+
+  def title=(name)
+    @title = titleize(name)
+  end
+
+  def titleize(word)
+    little_words = ["a", "and", "of", "over", "the", "in", "an"]
+    word.gsub(/\w+/).with_index {|match, i| little_words.include?(match) && i != 0 ? match : match.capitalize}
+  end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+
+  def initialize()
+  end
+
+  def seconds
+    @seconds = 0
+  end
+
+  def seconds=(time)
+    @seconds = Time.at(time - 3600)
+  end
+
+  def time_string
+     @seconds.strftime("%H:%M:%S")
+  end
 end
